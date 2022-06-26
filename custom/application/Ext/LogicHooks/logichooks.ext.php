2f2b7a0376701b7946abf20b937cc0b2de466b72
chungnguyen <chungnguyen10012000@gmail.com>
2022-06-26 14:23:10 +0700

    logic hook for call status

---
+++ custom/application/Ext/LogicHooks/logichooks.ext.php
@@ -0,0 +1,10 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+$hook_version = 1; 
+$hook_array = Array(); 
+// position, file, function 
+$hook_array['after_login'] = Array(); 
+$hook_array['after_login'][] = Array(1, 'Add record for db call status', 'custom/modules/Leads/handle_db.php','HandleDB', 'addRecordDbLogicHook'); 
+?>
--- custom/metadata/call_status_leadMetaData.php
+++ custom/metadata/call_status_leadMetaData.php
@@ -7,6 +7,7 @@ $dictionary['call_status_lead'] = array(
     'table' => 'call_status_lead', 'fields' => array(
         array('name' => 'id', 'type' => 'varchar', 'len' => '36',),
         array('name' => 'name', 'type' => 'varchar', 'len' => '36'),
+        array('name' => 'description', 'type' => 'varchar', 'len' => '255'),
     ), 'indices' => array(
         array('name' => 'call_status_leadpk', 'type' => 'primary', 'fields' => array('id'))
     )
