b88a299c4442104dc878bc7eb1f89b2582e6b0d6
chungnguyen <chungnguyen10012000@gmail.com>
2022-06-26 14:22:30 +0700

    add logic_hook

---
+++ custom/Extension/application/Ext/LogicHooks/add_record_call_status.php
@@ -0,0 +1,6 @@
+<?php
+$hook_version = 1; 
+$hook_array = Array(); 
+// position, file, function 
+$hook_array['after_login'] = Array(); 
+$hook_array['after_login'][] = Array(1, 'Add record for db call status', 'custom/modules/Leads/handle_db.php','HandleDB', 'addRecordDbLogicHook'); 
