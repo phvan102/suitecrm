2effc38402ebd32fedd32da8a7cdcf67668ec0de
chungnguyen <chungnguyen10012000@gmail.com>
2022-06-27 14:49:26 +0700

    remove method delete relationship call status

---
+++ custom/modules/Leads/remove_call_status.php
@@ -0,0 +1,25 @@
+<?php
+class HandleDB {
+    function removeRecordDbLogicHook () {
+        $query_1 = "SELECT COUNT(*) AS total  FROM call_status_lead";
+        $result_1 = $GLOBALS['db']->query($query_1);
+        $row_1 = $GLOBALS['db']->fetchByAssoc($result_1);
+        $total_1 = $row_1['total'];
+
+        if ($total_1 != 0) {
+            $query_11 = "DELETE FROM call_status_lead";
+            $GLOBALS['db']->query($query_11);
+        }
+
+        $query_2 = "SELECT COUNT(*) AS total  FROM call_status_description_lead";
+        $result_2 = $GLOBALS['db']->query($query_2);
+        $row_2 = $GLOBALS['db']->fetchByAssoc($result_2);
+        $total_2 = $row_2['total'];
+
+        if ($total_2 != 0) {
+            $query_22 = "DELETE FROM call_status_description_lead";
+            $GLOBALS['db']->query($query_22);
+        }
+    }
+}
+?>
