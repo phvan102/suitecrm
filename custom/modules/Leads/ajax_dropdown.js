f253c8193e45d1d1d6b496d4567a5459c9a2e2e3
chungnguyen <chungnguyen10012000@gmail.com>
2022-06-27 10:56:25 +0700

    edit data name, add show dropdown 2nd now of ajax handle show dropdown

--- custom/modules/Leads/ajax_dropdown.js
+++ custom/modules/Leads/ajax_dropdown.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    $('#call_status_lead').change(function() {
-        var x = $('#call_status_lead option:selected').val();
+    var x = $('#call_status_lead option:selected').val();
         $.ajax({
             url: "index.php?module=Leads&entryPoint=LeadCallStatus&case=1",
             data: {id: x},
@@ -10,5 +9,26 @@ $(document).ready(function() {
             },
             dataType: 'html'
         });
+    $('#call_status_lead').change(function() {
+        var call_status_lead_id = $('#call_status_lead option:selected').val();
+        $.ajax({
+            url: "index.php?module=Leads&entryPoint=LeadCallStatus&case=1",
+            data: {id: call_status_lead_id},
+            success: function(data){
+                console.log(data);
+                $("#call_status_description_lead").html(data);
+            },
+            dataType: 'html'
+        });
     });
+
+    $('#SAVE').click(function() {
+        var call_status_lead = $('#call_status_lead option:selected').val();
+        var call_status_description_lead = $('#call_status_description_lead option:selected').val();
+        console.log(call_status_lead, call_status_description_lead);
+        var _form = document.getElementById('EditView'); _form.action.value='Save'; 
+        if(check_form('EditView'))
+            SUGAR.ajaxUI.submitForm(_form);
+        return false;
+    })
 });
