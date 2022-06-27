39986b6ce9a2132bb3c7f23b4fec03bc08b3c2bb
chungnguyen <chungnguyen10012000@gmail.com>
2022-06-27 17:37:57 +0700

    add ajax send call_status_description_id

--- custom/modules/Leads/ajax_dropdown.js
+++ custom/modules/Leads/ajax_dropdown.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-    var x = $('#call_status_lead option:selected').val();
+    var call_status_lead_id = $('#call_status_lead option:selected').val();
+    var call_status_description_lead_id = $('#call_status_description_id').val();
         $.ajax({
             url: "index.php?module=Leads&entryPoint=LeadCallStatus&case=1",
-            data: {id: x},
+            data: {id: call_status_lead_id, call_status_description_lead_id: call_status_description_lead_id},
             success: function(data){
                 console.log(data);
                 $("#call_status_description_lead").html(data);
@@ -11,9 +12,11 @@ $(document).ready(function() {
         });
     $('#call_status_lead').change(function() {
         var call_status_lead_id = $('#call_status_lead option:selected').val();
+        var call_status_description_lead_id = $('#call_status_description_id').val();
+        console.log(call_status_description_lead_id)
         $.ajax({
             url: "index.php?module=Leads&entryPoint=LeadCallStatus&case=1",
-            data: {id: call_status_lead_id},
+            data: {id: call_status_lead_id, call_status_description_lead_id: call_status_description_lead_id},
             success: function(data){
                 console.log(data);
                 $("#call_status_description_lead").html(data);
