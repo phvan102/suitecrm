d45d03a3a7c4a1be09292d052c88441d49aa278c
chungnguyen <chungnguyen10012000@gmail.com>
2022-06-28 14:51:45 +0700

    fix width for field call_status_description_lead

--- custom/modules/Leads/ajax_dropdown.js
+++ custom/modules/Leads/ajax_dropdown.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
     var call_status_lead_id = $('#call_status_lead option:selected').val();
     var call_status_description_lead_id = $('#call_status_description_id').val();
-        $.ajax({
-            url: "index.php?module=Leads&entryPoint=LeadCallStatus&case=1",
-            data: {id: call_status_lead_id, call_status_description_lead_id: call_status_description_lead_id},
-            success: function(data){
-                console.log(data);
-                $("#call_status_description_lead").html(data);
-            },
-            dataType: 'html'
-        });
+    $(window).resize(function () {
+        if ($('#call_status_description_lead').length) {
+            let width_call_status_description_lead = $('#phone_mobile').width().toString() + "px";
+            $('#call_status_description_lead').css("width", width_call_status_description_lead);
+        }
+    })
+    $.ajax({
+        url: "index.php?module=Leads&entryPoint=LeadCallStatus&case=1",
+        data: {id: call_status_lead_id, call_status_description_lead_id: call_status_description_lead_id},
+        success: function(data){
+            console.log(data);
+            $("#call_status_description_lead").html(data);
+            let width_call_status_description_lead = $('#phone_mobile').width().toString() + "px";
+            $('#call_status_description_lead').css("width", width_call_status_description_lead);
+            
+        },
+        dataType: 'html'
+    });
     $('#call_status_lead').change(function() {
         var call_status_lead_id = $('#call_status_lead option:selected').val();
         var call_status_description_lead_id = $('#call_status_description_id').val();
@@ -20,6 +29,8 @@ $(document).ready(function() {
             success: function(data){
                 console.log(data);
                 $("#call_status_description_lead").html(data);
+                let width_call_status_description_lead = $('#phone_mobile').width().toString() + "px";
+                $('#call_status_description_lead').css("width", width_call_status_description_lead);
             },
             dataType: 'html'
         });
