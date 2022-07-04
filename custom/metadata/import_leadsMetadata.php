235fbaf397cf49c77b5d75710f040518cb18bf0b
chungnguyen <chungnguyen10012000@gmail.com>
2022-07-04 11:52:17 +0700

    add auto increment id, field date_post

--- custom/metadata/import_leadsMetadata.php
+++ custom/metadata/import_leadsMetadata.php
@@ -8,8 +8,9 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $dictionary['import_leads'] = array(
     'table' => 'import_leads', 'fields' => array(
-        array('name' => 'id', 'type' => 'int'),
+        array('name' => 'id', 'type' => 'int', 'auto_increment' => true),
         array('name' => 'date_updated', 'type' => 'varchar', 'len' => '255'),
+        array('name' => 'date_post', 'type' => 'varchar', 'len' => '255'),
         array('name' => 'user_updated', 'type' => 'varchar', 'len' => '255' ),
         array('name' => 'link_file', 'type' => 'varchar', 'len' => '255' ),
         array('name' => 'number_import_in_day', 'type' => 'int'),
